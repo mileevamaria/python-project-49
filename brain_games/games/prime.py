682bc2966e9e8636fb2e4676aa955298cc9dfc4f
Maria Mileeva <mileeva.mary@gmail.com>
2026-03-04 17:22:12 +0300

    fix brain-prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -14,9 +14,10 @@ def generate_number_for_prime() -> tuple[str, str]:
     """
     number = random.randint(PRIME_MIN, PRIME_MAX)
     is_prime = True
-    for delimetr in range(2, number // 2 + 1):
-        if number % delimetr == 0:
-            is_prime = False
-            break
+    if not (number == 1 or number == 2):    
+        for delimetr in range(2, number // 2 + 1):
+            if number % delimetr == 0:
+                is_prime = False
+                break
     is_prime_str = 'yes' if is_prime else 'no'
     return str(number), is_prime_str
