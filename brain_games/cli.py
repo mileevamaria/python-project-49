d6f7aebe0bb577d705cf99e4cb97823823dc37ab
Maria Mileeva <mileeva.mary@gmail.com>
2026-03-04 15:04:53 +0300

    bugfix

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,5 +3,6 @@ import prompt
 
 def welcome_user():
     """Приветствие пользователя"""
+    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name.capitalize()}!')
